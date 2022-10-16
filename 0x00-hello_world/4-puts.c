aa4f32ee88fb43af3f23efcd2359cdbb618c129b
Ephraim67 <jonathaneuphrem@gmail.com>
2022-10-16 04:23:18 -0400

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,11 @@
  * main - Entry poin
  *
  * Return: always 0 (success)
+ * 
  * */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
