fe98240e5a1f77975ec3f4735ef2b513aa5c391e
Ephraim67 <jonathaneuphrem@gmail.com>
2023-05-12 07:14:51 -0400

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,18 @@
 /**
  * main - Entry point
  *
- * Return: 1
+ * Description: Prints a string to the standard error
+ * Return: Always 1 (Error)
  */
 
 int main(void)
 {
-    const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(1, str, 59);
-    return (1);
+        const char *str = "and that piece of art is useful\" "
+                          "- Dora Korpar, 2015-10-19\n";
+
+        /* write the string to the standard error (file descriptor 2) */
+        write(2, str, 59);
+
+        return (1);
 }
 
