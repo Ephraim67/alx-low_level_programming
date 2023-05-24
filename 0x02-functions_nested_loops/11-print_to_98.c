e7899e5bb1620bd71a6a63ddb6d717f352459f6e
Ephraim67 <jonathaneuphrem@gmail.com>
2023-05-24 07:52:25 -0400

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,18 +6,27 @@
  *
  * @n: The starting number.
  */
-void print_to_98(int n) {
-    if (n <= 98) {
-        for (; n <= 98; n++) {
+void print_to_98(int n) 
+{
+    if (n <= 98) 
+    {
+        for (; n <= 98; n++) 
+	{
             printf("%d", n);
-            if (n != 98) {
+            if (n != 98) 
+	    {
                 printf(", ");
             }
-        }
-    } else {
-        for (; n >= 98; n--) {
+	}
+    }
+
+    else 
+    {
+        for (; n >= 98; n--) 
+	{
             printf("%d", n);
-            if (n != 98) {
+            if (n != 98) 
+	    {
                 printf(", ");
             }
         }
