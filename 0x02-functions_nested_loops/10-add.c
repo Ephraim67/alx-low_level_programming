581cda5d0891d8a4b7b05ea0862fa5d9f0c25ee2
Ephraim67 <jonathaneuphrem@gmail.com>
2023-05-24 05:36:36 -0400

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add two interger and return the result
+ *@a - first interger to be added
+ *@b - second interger to be added
+ *Return: the result adding the two integer
+ */
+
+int add(int a, int b) {
+    int sum = a + b;
+    return sum;
+}
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,36 +8,36 @@
  */
 void times_table(void)
 {
-    int i, j, n;
+        int i, j, n;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            n = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			n = i * j;
 
-            if ((n / 10) == 0)
-            {
-                if (j != 0)
-                    putchar(' ');
-                putchar(n + '0');
+			if ((n / 10) == 0)
+			{
+				if (j != 0)
+					putchar(' ');
+				putchar(n + '0');
 
-                if (j == 9)
-                    continue;
-                putchar(',');
-                putchar(' ');
-            }
-            else
-            {
-                putchar((n / 10) + '0');
-                putchar((n % 10) + '0');
-                if (j == 9)
-                    continue;
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        putchar('\n');
-    }
+				if (j == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+				if (j == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
 }
 
