d8dd5d0c2e97e5cd9946c7a8e643ca8c6d8dfbc3
Ephraim67 <jonathaneuphrem@gmail.com>
2023-05-24 07:48:21 -0400

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints numbers from a given number up to 98.
+ *
+ * @n: The starting number.
+ */
+void print_to_98(int n) {
+    if (n <= 98) {
+        for (; n <= 98; n++) {
+            printf("%d", n);
+            if (n != 98) {
+                printf(", ");
+            }
+        }
+    } else {
+        for (; n >= 98; n--) {
+            printf("%d", n);
+            if (n != 98) {
+                printf(", ");
+            }
+        }
+    }
+    printf("\n");
+}
